9c5ac27e3e52e7f038d7c6ade321c4659a44d43e
Paulson20705 <103404344+Paulson20705@users.noreply.github.com>
2023-08-02 19:53:22 +0530

    Add files via upload

--- 28.07.2023/Server/server.js
+++ 28.07.2023/Server/server.js
@@ -6,6 +6,7 @@ const con = require('./connection.js')
 const cors = require('cors')
 let bcrypt = require('bcrypt');
 const { error } = require('console');
+const { title } = require('process');
 
 
 // app.use( {
@@ -58,8 +59,12 @@ app.post('/signup', (req, res) => {
             } else {
               res.json({ message: 1 })
               let tab_name = req.body.email.substring(0, req.body.email.indexOf("@"))
+              let tab_name2 = req.body.email.substring(0, req.body.email.indexOf("@")) + "page"
               con.query(
                 `CREATE TABLE ${tab_name} (con_num INT(10) PRIMARY KEY AUTO_INCREMENT,title VARCHAR(50),content LONGTEXT)`
+              )
+              con.query(
+                `CREATE TABLE ${tab_name2} (title VARCHAR(50))`
               )
             }
           })
@@ -127,6 +132,40 @@ app.post('/postbar', (req, res) => {
     })
 })
 
+app.post('/savepost', (req, res) => {
+
+  let contentTitle = ''
+  let emailId = req.body.email
+  let TableName = emailId.substring(0, emailId.indexOf("@"))
+  let content = req.body.content
+  let Untitled = "Untitled"
+
+  if (req.body.title.length < 1) {
+    contentTitle = Untitled
+  } else {
+    contentTitle = req.body.title
+  }
+
+  con.query(
+    `select title from ${TableName} where title= '${contentTitle}'`,
+    function (err, result) {
+      if (err) {
+        console.log(err)
+      }
+      if (result.length >= 1) {
+        con.query(
+          `update ${TableName} set content="${content}" where title = '${contentTitle}'`
+        )
+      } else {
+        con.query(
+          `insert into ${TableName} values (null,'${contentTitle}','${content}')`
+        )
+      }
+    }
+  )
+
+})
+
 app.post('/deletepost', (req, res) => {
   let validEmail = req.body.validEmail
   let TitleName = req.body.titleName
@@ -134,7 +173,7 @@ app.post('/deletepost', (req, res) => {
 
   con.query(
     `delete from ${TableName} where title = '${TitleName}'`,
-    function (err, res) {
+    function (err, result) {
       if (err) {
         console.log(err)
       } else {
@@ -144,6 +183,73 @@ app.post('/deletepost', (req, res) => {
   )
 })
 
-app.listen(100, () => {
+app.post('/sendCon', (req, res) => {
+  let Title = req.body.title
+  let validEmail = req.body.email
+  let TableName = validEmail.substring(0, validEmail.indexOf("@"))
 
+  con.query(
+    `select content from ${TableName} where title = '${Title}'`,
+    function (err, result) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(result)
+        res.json({ content: result })
+      }
+    }
+  )
 })
+
+app.post('/pagebar', (req, res) => {
+  const validEmail = req.body.validEmail;
+  let TableName = validEmail.substring(0, validEmail.indexOf("@")) + "page"
+
+  con.query(
+    `select title from ${TableName}`,
+    function (err, result) {
+      if (result.length >= 1) {
+        res.json({ message: result })
+      } else {
+        res.json({ message: 0 })
+      }
+    }
+  )
+})
+
+app.post('/savepage', (req, res) => {
+  const validEmail = req.body.email;
+  const value = req.body.value;
+
+  let TableName = validEmail.substring(0, validEmail.indexOf("@")) + "page"
+
+  con.query(
+    `select title from ${TableName} where title='${value}'`,
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+        if (result.length >= 1) {
+          console.log("There is Value");
+        } else {
+          con.query(
+            `insert into ${TableName} values ('${value}')`
+          )
+        }
+      }
+    }
+  );
+})
+
+app.post('/deletepage', (req, res) => {
+  let validEmail = req.body.validEmail
+  let TitleName = req.body.title
+  let TableName = validEmail.substring(0, validEmail.indexOf("@")) + "page";
+
+  con.query(
+    `delete from ${TableName} where title = '${TitleName}'`
+  )
+})
+
+app.listen(100, () => { })
